a0693fbeb43ddd71576f8b9ce6e76722b53cf77e
Reedo0910 <sea.sand@live.cn>
2019-03-24 23:38:59 -0400

    bug fix

--- src/components/KeyPanel.vue
+++ src/components/KeyPanel.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="key-panel" :class="panelName" v-show="!value || activePanel === panelName">
-    <label :for="panelName" class="center-key" :data-keyname="getFirstLetter(panelKeys)" @click="SetPanel"></label>
+    <label
+      :for="panelName"
+      class="center-key"
+      :data-keyname="getFirstLetter(panelKeys)"
+      @click="SetPanel"
+    ></label>
     <div class="cssplay-menu">
       <div>
         <input
@@ -62,7 +67,8 @@
         this.$emit('setactive', false);
       },
       getFirstLetter: function (keys) {
-        const tarArr = keys.sort();
+        const copy = keys.concat();
+        const tarArr = copy.sort();
         return tarArr[0];
       }
     }
@@ -144,7 +150,7 @@
     overflow: hidden;
     transform: scale(0);
     /* transition: 0.5s cubic-bezier(.58, 2.4, 0.5, 0.5);
-                                                                                transition: 0.5s ease; */
+                                                                                  transition: 0.5s ease; */
   }
 
   .cssplay-menu .segment button {
@@ -160,7 +166,7 @@
     cursor: pointer;
     transform-origin: left top;
     /* transition: 0.25s cubic-bezier(0, 0, 1, 1);
-                                                                                transition: 0.25s ease; */
+                                                                                  transition: 0.25s ease; */
   }
 
   //   .cssplay-menu .segment label .ph {
@@ -215,7 +221,7 @@
 
   .cssplay-menu .key-input:checked + .holder .segment {
     /* transition: 0.5s cubic-bezier(.58, 3, 0.5, 0.5);
-                                                                                transition: 0.5s ease; */
+                                                                                  transition: 0.5s ease; */
     transform: scale(1);
   }
 
