34c73a28b719da282b19159a9a18177bb4f4a88b
Reedo0910 <sea.sand@live.cn>
2019-03-25 01:26:04 -0400

    add code and then remove it

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,25 @@
     },
     beforeMount() {
       document.body.addEventListener('touchstart', function () { });
+
+      // document.addEventListener('touchstart', function (event) {
+      //   if (event.touches.length > 1) {
+      //     event.preventDefault();
+      //   }
+      // });
+
+      // let lastTouchEnd = 0;
+      // document.addEventListener('touchend', function (event) {
+      //   let now = (new Date()).getTime();
+      //   if (now - lastTouchEnd <= 100) {
+      //     event.preventDefault();
+      //   }
+      //   lastTouchEnd = now;
+      // }, false);
+
+      // document.addEventListener('gesturestart', function (event) {
+      //   event.preventDefault();
+      // });
     },
     beforeDestroy() {
       document.body.removeEventListener('touchstart', function () { });
